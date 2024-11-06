949740e1bbaf645db0f1641cbb97cff50d90def9
LauraDaniFlores <laura.dani.flores@gmail.com>
2024-11-06 10:51:07 -0600

    Actualización de los datos.

--- php/peliculas.php
+++ php/peliculas.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GoodWatch | Peliculas</title>
+    <title>GoodWatch | Películas</title>
 
     <!-- Estilos -->
     <link rel="stylesheet" href="../css/peliculas.css">
