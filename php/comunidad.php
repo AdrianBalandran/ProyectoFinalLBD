a07474c4b1655049869733b75d4669f2c1c4e596
DianaGabiFlores <diana.gabi.flores@gmail.com>
2024-11-21 13:36:22 -0600

    Funcionalidad de comunidad
    Solo falta ajustar un poco el layout y la funcionalidad de los comentarios.

--- php/comunidad.php
+++ php/comunidad.php
@@ -1,6 +1,14 @@
 <?php 
     session_start();
     date_default_timezone_set('America/Mexico_City');
+
+    $servidor='localhost';
+    $cuenta='root';
+    $password='';
+    $bd='GOODWATCH';
+
+    //conexion a la base de datos
+    $conexion = new mysqli($servidor, $cuenta, $password, $bd);
 ?>
 
 <!DOCTYPE html>
@@ -31,50 +39,7 @@
     <section class="contenido">
         <section class="comunidad">
 
-
-            <div class="update">
-
-                <div class="head">
-                    <img src="../imagenes/faviconi.png" alt="perfil" class="usuario">
-                    <div>
-                        <h5>Nombre Watched a film</h5>
-                        <p>Fecha</p>
-                    </div>
-                    <p>Estrellitas</p>
-
-                </div>
-
-                <div class="comentario">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo quaerat molestias tempore cumque incidunt nemo corporis inventore, aut laudantium, quas debitis alias consectetur nihil officiis suscipit ipsum sed autem facilis!</p>
-                </div>
-
-                <div class="peliculaInfo">
-                    <img src="../imagenes/emma.jpg" alt="">
-                    <div class="description">
-                        <div class="nomIcono">
-                            <h2>Nombre</h2>
-                            <img src="../imagenes/faviconi.png" alt="">
-                        </div>
-                       <div class="direcFecha">
-                            <h5>Director</h5>
-                            <p>anio</p>
-                       </div>
-                    
-                        <p>Descripcion</p>
-                    </div>
-                </div>
-                <div class="fav_com">
-                    <div class="megustas">
-                        <img src="../imagenes/recursos/heart.png" alt="">
-                        <p>1</p>
-                    </div>
-                    <div class="comentarios">
-                        <img src="../imagenes/recursos/comment.png" alt="">
-                        <p>3</p>
-                    </div>
-
-                </div>
-            </div>
+        <?php datos($conexion); ?>
 
         </section>
         <section class="mas">
@@ -88,60 +53,140 @@
 
 
 <?php 
-    $servidor='localhost';
-    $cuenta='root';
-    $password='';
-    $bd='GOODWATCH';
 
-    //conexion a la base de datos
-    $conexion = new mysqli($servidor, $cuenta, $password, $bd);
 
-    function datos($conexion, $id, $temporada){
-        $sql = "SELECT FI.NOMBRE, DESCRIPCION, SE.FECHA_ESTRENO, CLASIFICACION, ID.NOMBRE IDIOMA, PA.NOMBRE PAIS, SE.IMAGENTEM, SE.NUMERO_EPISODIOS NE FROM FILME FI, IDIOMA ID, PAIS PA, SERIE SE WHERE FI.ID_FILME = '$id' AND FI.ID_FILME = SE.ID_FILME AND SE.TEMPORADA = '$temporada' AND FI.ID_IDIOMA = ID.ID_IDIOMA AND FI.ID_PAIS = PA.ID_PAIS;";
+    function datos($conexion){
+        $sql = "SELECT USU.NOMBRE NOMBRE, VI.ID_FILME FILME, VI.CALIFICACION CAL, VI.FECHA_VISUALIZACION VISU, VI.OPINION OPINION, PLA.NOMBRE NOMPLA, IDI.NOMBRE NOMIDI, FILME.NOMBRE NOMFILM, FILME.FECHA_ESTRENO ESTRENO, FILME.DESCRIPCION DES,
+            VI.NUM_LIKES LIKES, IDIOMA.NOMBRE IDIOMA, VI.ID_FILME_SE ID, VI.TEMPORADA TEMP
+            FROM VISUALIZACION VI, USUARIO USU, PLATAFORMA PLA, IDIOMA IDI, FILME, IDIOMA
+            WHERE VI.ID_USUARIO = USU.ID_USUARIO
+            AND PLA.ID_PLAT = VI.ID_PLATAFORMA
+            AND IDI.ID_IDIOMA = VI.ID_IDIOMA
+            AND FILME.ID_FILME = VI.ID_FILME
+            AND IDIOMA.ID_IDIOMA = VI.ID_IDIOMA;";
+
         $resultado = $conexion -> query($sql);
-        while( $fila = $resultado -> fetch_assoc() ){
-    
-        }
-    }
 
-?>
+        while( $fila = $resultado -> fetch_assoc() ){ ?>
 
-<!-- 
-<div class="estrellas"> <?php
-                        $sql1 = "SELECT ROUND(AVG(CALIFICACION), 1) CALI FROM VISUALIZACION WHERE ID_FILME = '$id';";
-                        $resultado1 = $conexion -> query($sql1);
-                        if ($resultado1->num_rows > 0) {
-                        while( $fila1 = $resultado1 -> fetch_assoc() ){
-                            if($fila1['CALI'] != NULL){
-                            for ($i = 1; $i <= (int)$fila1['CALI']; $i++) { ?>
+            <div class="update">
+
+                <div class="head">
+                    <div class="imgusuario">
+                        <img src="../imagenes/recursos/usuario.png" alt="perfil" class="usuario">
+                    </div>
+                    <div class="nombreVisu">
+                        <h5><span class="color"> <?php echo $fila['NOMBRE'] ?></span> Watched a film</h5>
+                        <p><span class="color">Fecha: </span><?php echo $fila['VISU'];?> <span class="idioma"><span class="color">Idioma: </span><?php echo $fila['IDIOMA'];?></span> </p>
+                    </div>
+                    <div class="estrellas"> <?php
+                            if($fila['CAL'] != NULL){
+                            for ($i = 1; $i <= (int)$fila['CAL']; $i++) { ?>
                                 <img src="../imagenes/recursos/start.png" alt="Estrellas">
                             <?php }
-                            $num1 = $fila1['CALI'];
-                            $num = (float)$fila1['CALI'] - (int)$fila1['CALI']; 
-                            $num = (float)$num;
-                            $num = strval($num); 
-                            if($num >= 0.1 && $num <= 0.2){ ?>
-                                <img src="../imagenes/recursos/start2.png" alt="Estrellas" class="media2">
-                            <?php }else if($num >= 0.3 && $num <= 0.4){
-                                ?>
-                                <img src="../imagenes/recursos/start4.png" alt="Estrellas" class="media4">
-                                <?php
-                            }else if($num == 0.5){
-                                ?>
-                                <img src="../imagenes/recursos/startmedia.png" alt="Estrellas" class="media">
-                                <?php
-                            }else if($num >= 0.6 && $num <= 0.7){
-                                ?>
-                                <img src="../imagenes/recursos/start6.png" alt="Estrellas" class="media6">
-                                <?php
-                            }else if($num >= 0.8 && $num <= 0.9){
-                                ?>
-                                <img src="../imagenes/recursos/start8.png" alt="Estrellas" class="media8">
-                                <?php
-                            }
-                            } ?>
-                            <?php
-                        }
-                    }else{
-                        }?> 
-                        </div> -->
+                             
+                            for ($i = 1; $i <= 5-(int)$fila['CAL']; $i++) { ?>
+                                <img src="../imagenes/recursos/starwhite.png" alt="Estrellas">
+                            <?php }
+                            }?>
+                    
+                        </div>
+                </div>
+
+                <div class="comentario">
+                    <p><?php echo $fila['OPINION'] ?> </p>
+                </div>
+
+                <div class="peliculaInfo">
+                    <?php if($fila['ID'] == NULL){
+                        $ID = $fila['FILME'];
+                         $sql = "SELECT FILME.IMAGEN IMG, P.DURACION DUR FROM FILME, PELICULA P 
+                            WHERE P.ID_FILME = FILME.ID_FILME AND FILME.ID_FILME=$ID;";
+                     $resultado1 = $conexion -> query($sql);
+             
+                     while( $fila1 = $resultado1 -> fetch_assoc() ){
+                        $IMG = $fila1['IMG'];
+                        $DUR = $fila1['DUR'] . " min";
+                     }
+                    }else {
+                        $ID = $fila['ID'];
+                        $TEMP = $fila['TEMP'];
+                         $sql = "SELECT SERIE.NUMERO_EPISODIOS EP, SERIE.IMAGENTEM IMG, SERIE.FECHA_ESTRENO TEMPFECHA
+                         FROM SERIE WHERE SERIE.ID_FILME = $ID, SERIE.TEMPORADA = $TEMP;";
+                        $resultado1 = $conexion -> query($sql);
+             
+                         while( $fila1 = $resultado1 -> fetch_assoc() ){
+                            $IMG = $fila1['IMG'];
+                            $DUR = $fila1['EP'] . " episodios";
+                            $TEMPFECHA = $fila1['TEMPFECHA'];
+                     }
+                    }?>
+                    <div class="imgPeli">
+                        <img src="../imagenes/<?php echo $IMG; ?>" alt="">
+                    </div>
+                    <div class="description">
+                        <div class="nomIcono">
+                            <h2> <?php echo $fila['NOMFILM']; if($fila['ID'] != NULL){echo "  " . $fila['TEMP']; }?> </h2>
+                            <?php $ruta = "../imagenes/plataformas/" . plataforma($fila['NOMPLA']);?>                        
+                            <img src="<?php echo $ruta?>" alt="">
+                        </div>
+                       <div class="direcFecha">
+                            <h6><?php echo $DUR ?></h6>
+                            <p><?php if($fila['ID'] == NULL){echo $fila['ESTRENO'];}else{ echo $TEMPFECHA; } ?></p>
+                       </div>
+                    
+                        <p><?php echo $fila['DES'] ?></p>
+                    </div>
+                </div>
+                <div class="fav_com">
+                    <div class="megustas">
+                        <?php $like = 0;
+                        if($fila['LIKES'] != NULL) {
+                            $like = $fila['LIKES'];
+                        }?>
+                        <img src="../imagenes/recursos/whiteheart.png" alt="">
+                        <p><?php echo $like;?></p>
+                    </div>
+                    <div class="comentarios">
+                        <img src="../imagenes/recursos/comment.png" alt="">
+                        <p>3</p>
+                    </div>
+
+                </div>
+            </div>
+            
+    <?php }
+    }   
+    
+    
+    function plataforma($plat){
+        switch ($plat) {
+            case "Netflix": 
+                $ruta = "netflix.png";
+                break;
+            case "Prime Video": $ruta = "primeVideo.png";
+                break;
+            case "Max": $ruta = "max.png";
+                break;
+            case "Apple Tv": $ruta = "appleTv.png";
+                break;
+            case "Disney Plus": $ruta = "disney.png";
+                break;    
+            case "YouTube": $ruta = "youtube.png";
+                break;
+            case "VIX": $ruta = "vix.png";
+                break;
+            case "Paramount Plus": $ruta = "paramount.jpg";
+                break;
+            case "Star Plus": $ruta = "star.jpeg";
+                break;
+            case "¡QIYI!": $ruta = "qiyi.png";
+                break;              
+            case "Viki Rakuten": $ruta = "viki.png";
+                break;
+            case "WeTV": $ruta = "weTV.jpg";
+                break;  
+        }   
+        return $ruta;     
+    }
+?>
